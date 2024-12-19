c7629b49cf87c8e909e3564429b983c9b45d30a8
saraHovagimian <shovagimian@bas.com.ar>
2024-12-19 16:06:12 -0300

    modificaciones de recordatorios que aparecen en el aside 19-12-24

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/your_Easysoft_world.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '8722a387-6a76-4871-97fa-1159b132a1f2' || 'preview');
+rh.consts('HELP_ID', '7d29ad98-374e-4544-b1fb-d1cf25185971' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
