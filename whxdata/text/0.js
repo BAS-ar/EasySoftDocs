be555ebe4aac85526eb5eafe5dc5512f0802dfd0
saraHovagimian <shovagimian@bas.com.ar>
2025-06-06 13:16:40 -0300

    modificacion titulo balances y centros de costos globales 06-06-25

--- whxdata/text/0.js
+++ whxdata/text/0.js
@@ -1,1 +1,1 @@
-rh._.exports({"0":[["exaFooter"]],"1":[[" ","EasySoft"," ","v:","1.0.0.0 - 21/05/2025"," ","-"," ","Powered by"," ","-"," ","Términos y condiciones."]],"id":"0"})
+rh._.exports({"0":[["exaFooter"]],"1":[[" ","EasySoft"," ","v:","1.0.0.0 - 06/06/2025"," ","-"," ","Powered by"," ","-"," ","Términos y condiciones."]],"id":"0"})
