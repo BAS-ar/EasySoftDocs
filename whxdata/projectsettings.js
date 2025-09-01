ae99bbfa6d11d420f875f8a287e3a22dd6342a0c
saraHovagimian <shovagimian@bas.com.ar>
2025-09-01 15:06:23 -0300

    Modificacion de listados Diario y Mayor 01-09-25

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'b4bcd671-cb57-4bac-950c-be6687ef8ce7' || 'preview');
+rh.consts('HELP_ID', '956b3254-f163-4b30-861d-93f60d2d18f3' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
