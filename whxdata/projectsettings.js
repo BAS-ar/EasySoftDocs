ecd059a38b1fe051bdde81a11ce9288653c5c078
saraHovagimian <shovagimian@bas.com.ar>
2025-09-17 16:05:29 -0300

    Campo cantidad en balances 17-09-25

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '3f18b415-4950-456e-b39f-e2160abc5d55' || 'preview');
+rh.consts('HELP_ID', 'e3d56d21-130e-431b-9aba-f059be924e12' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
