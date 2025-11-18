0d36ec641b64f7edc0a3ba0ec0f633da45f640fe
saraHovagimian <shovagimian@bas.com.ar>
2025-11-18 16:42:51 -0300

    eliminacion del chatbot del centro de aprendizaje 181125

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '69a11ad0-5df3-4168-90b6-8fa67180425f' || 'preview');
+rh.consts('HELP_ID', '75cb8bf4-c7c5-461f-9840-82948bb2ccf7' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
