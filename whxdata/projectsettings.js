42d6aca2e8e78807808f1802c485027a0c349efc
saraHovagimian <shovagimian@bas.com.ar>
2025-12-11 08:36:19 -0300

    extension subdiario 111225

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '037425a2-30dc-4475-a342-064158e02435' || 'preview');
+rh.consts('HELP_ID', 'a9547749-3605-47c7-a541-bce7c7ebea6f' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
