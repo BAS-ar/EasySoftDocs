459f2fd088e3bb6ca979d2e5d6f0b53ffb4390e1
saraHovagimian <shovagimian@bas.com.ar>
2026-01-29 09:46:25 -0300

    Subdiarios #638 Compras y Ventas #634 635  Reestructuracion de menu #449 290126

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '5209deb9-1f1d-4d7a-8f43-50df0f4957a3' || 'preview');
+rh.consts('HELP_ID', '2f218db8-dd1a-43c2-b900-0ba46d7ab4ba' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
