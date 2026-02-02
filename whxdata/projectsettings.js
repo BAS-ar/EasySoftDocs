8f1c052573e3769d38e4faa7d1ea0171321f08ac
saraHovagimian <shovagimian@bas.com.ar>
2026-02-02 17:57:18 -0300

    actualizacion en Github 020226

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '4bb3498a-0ad6-4445-9de1-63bd7c16b9de' || 'preview');
+rh.consts('HELP_ID', 'cb6eef71-6822-41ed-b822-d91637fe9100' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
