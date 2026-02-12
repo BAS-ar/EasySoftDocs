c1aa78e0566c9bb06095a802190fde7f08f9a77c
saraHovagimian <shovagimian@bas.com.ar>
2026-02-12 16:58:20 -0300

    Modificacion de pantallas comprobantes de ventas y de compras issue 646 120226

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '60cc55a3-fad5-4d8e-ba18-665e6dee91f7' || 'preview');
+rh.consts('HELP_ID', 'dfab3bbe-5ce6-473f-a449-a6573571457c' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
--- whxdata/text/6.js
+++ whxdata/text/6.js
@@ -1,1 +1,1 @@
-rh._.exports({"0":[[" ","VerModifVistas"]],"id":"6"})
+rh._.exports({"0":[[" ","AnoFiscalModif"]],"id":"6"})
