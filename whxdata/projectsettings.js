83a80a42b2386d19a32c50a055d53c94949d87fc
saraHovagimian <shovagimian@bas.com.ar>
2026-03-17 14:36:03 -0300

    modificacion de prueba 170326

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'caa033ac-81f4-42f6-b927-5cf5788f7d7a' || 'preview');
+rh.consts('HELP_ID', '2bca0fa7-66d6-45b5-95b4-b2ec0dcc7600' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
