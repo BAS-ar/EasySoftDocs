0571a180e264dc3eeb4f7d64e4137c13abf86835
saraHovagimian <shovagimian@bas.com.ar>
2026-04-09 12:36:05 -0300

    Emision subdiarios 684 Totales diario 314 y importacion arca 567 090426

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '5a7e41d8-5acc-4e07-bb14-fbdc2c479abd' || 'preview');
+rh.consts('HELP_ID', '854bd243-4473-437e-9259-4d4ffcce3b14' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
