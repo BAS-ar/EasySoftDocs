739fad2c33a81453322898b238bb459d49507a3d
saraHovagimian <shovagimian@bas.com.ar>
2026-06-18 11:57:13 -0300

    issue #441 eliminacion de cuentas 18-06-26

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', 'e50fecc0-4a81-4519-8bba-c026aa745169' || 'preview');
+rh.consts('HELP_ID', '295b0a80-0dbf-4f75-8e08-68ccfe6cbafe' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
