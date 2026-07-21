769b31c3e0109b9847d9bf20732c7588d16f9283
saraHovagimian <shovagimian@bas.com.ar>
2026-07-21 10:48:34 -0300

    issue 793 correccion en reversion de asientos, 729 condicion de iva en comprobantes vta 801 cambio de descripcion x detalle en asientos 21-07-26

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/user_profile.htm"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '1368ff42-76b7-470b-898b-5c78989e5269' || 'preview');
+rh.consts('HELP_ID', '8eeaf564-0268-43a9-b62b-ba3beaa62838' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
